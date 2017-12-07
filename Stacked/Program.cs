d2ecf612b25ebe52022db9fda3e3eb0bcf2bfbd4
Arshia <arshiakohanteb@gmail.com>
2017-12-06 16:48:35 -0800

    Finished Stacked, Fixel Inumerable Printing

--- Stacked/Program.cs
+++ Stacked/Program.cs
@@ -7,31 +7,42 @@ using System.Threading.Tasks;
 namespace Stacked
 {
     class Program 
-    {/// <summary>
-     /// Make These Fuctions 
-     /// https://github.com/GreatMindsRobotics/DataStructuresCurriculum/wiki/Stacks#basic-set-of-functions
-     /// </summary>
+    {
         static void Main(string[] args)
         {
-            
+            #region Creating The Linked List
             StackedLinkedList<int> StackedLinkedList = new StackedLinkedList<int>();
-
-            for (int i = 1; i <= 5; i++)
+            #endregion
+            #region Pushing
+            for (int i = 1; i <= 6; i++)
             {
                 StackedLinkedList.Push(i);
                 Console.WriteLine(i);
             }
             Console.WriteLine();
-            
-            
+            #endregion
+            #region Printing
+            foreach (StackedNode<int> node in StackedLinkedList.Print())
+            {
+                Console.WriteLine(node.Data);
+            }
+            Console.WriteLine();
+            #endregion
+            #region Popping
             for (int i = 0; i < 5; i++)
             {
                 var POP = StackedLinkedList.Pop();
                 Console.WriteLine(POP);                
             }
-                
-                   
-            
+            Console.WriteLine();
+            #endregion
+            #region Printing
+            foreach (StackedNode<int> node in StackedLinkedList.Print())
+            {
+                Console.WriteLine(node.Data);
+            }
+            Console.WriteLine();
+            #endregion
             Console.ReadKey();
         }
     }
